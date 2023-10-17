61fe5ec36170ddc7bd0cabc4d0c47e42a30df646
Eli Michael <michaelet@students.osd.wednet.edu>
2023-10-16 17:18:27 -0700

    Added API key

--- app.py
+++ app.py
@@ -1,1 +1,5 @@
 import requests
+
+key = "V86v838SJb4GJhpaNbElRqLSLHFhyBc0LPBscDetwnXZosPS2pmtehPSNNsY6Hy1"
+
+print(requests.get("https://www.thebluealliance.com/api/v3/status", headers={"X-TBA-Auth-Key": key}).text)
