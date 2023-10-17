a8ac0b7ff7c166f8557f1677b860ad443a5c1052
Eli Michael <michaelet@students.osd.wednet.edu>
2023-10-16 18:07:17 -0700

    Got pandas set up

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
+# Task: find all the teams 4450 will be upagainst by finding each event 4450 is competing in and looking at those teams
+
 import requests
+import pandas
+import json
 
 key = "V86v838SJb4GJhpaNbElRqLSLHFhyBc0LPBscDetwnXZosPS2pmtehPSNNsY6Hy1"
 
-print(requests.get("https://www.thebluealliance.com/api/v3/status", headers={"X-TBA-Auth-Key": key}).text)
+def getData(url):
+    response = requests.get(url, headers={"X-TBA-Auth-Key": key})
+    response = json.loads(response.text)
+    return pandas.DataFrame.from_records(response)
+
+print(getData("https://www.thebluealliance.com/api/v3/events/2023").query(""))
