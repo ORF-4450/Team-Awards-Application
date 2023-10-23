f5e1370f4452f203e8df72992ad8bbf98e26801e
tealcoderotis <parallelcircuit17@gmail.com>
2023-10-22 17:25:45 -0700

    Got all the teams orf4450 has competed in for the inputed year

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 # Task: find all the teams 4450 will be upagainst by finding each match frc4450 had competed in and look up those matches to find the teams in those matches
 
 import requests
-import pandas
 import json
 
 key = "V86v838SJb4GJhpaNbElRqLSLHFhyBc0LPBscDetwnXZosPS2pmtehPSNNsY6Hy1"
@@ -9,6 +8,26 @@ key = "V86v838SJb4GJhpaNbElRqLSLHFhyBc0LPBscDetwnXZosPS2pmtehPSNNsY6Hy1"
 def getData(url):
     response = requests.get(url, headers={"X-TBA-Auth-Key": key})
     response = json.loads(response.text)
-    return pandas.DataFrame.from_records(response)
+    return response
 
-print(getData("https://www.thebluealliance.com/api/v3/events/2023").query(""))
+year = input("What year do you want to know the teams orf4450 has competed in: ")
+print("Please wait...")
+
+matches = getData("https://www.thebluealliance.com/api/v3/team/frc4450/matches/" + year)
+
+teams = []
+
+print("")
+
+for match in matches:
+    redAllianceTeams = match["alliances"]["red"]["team_keys"]
+    for team in redAllianceTeams:
+        if (not team in teams) and team != "frc4450":
+            teams.append(team)
+    blueAllianceTeams = match["alliances"]["blue"]["team_keys"]
+    for team in blueAllianceTeams:
+        if (not team in teams) and team != "frc4450":
+            teams.append(team)
+
+for team in teams:
+    print(team)
