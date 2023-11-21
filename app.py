ae5bd509c1d89da5820dab79e2aab5c3a2a29ae8
Eli Michael <michaelet@students.osd.wednet.edu>
2023-11-20 17:00:47 -0800

    Multithreaded the requests for the rewards

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 
 import requests
 import json
+import threading
 
 key = "V86v838SJb4GJhpaNbElRqLSLHFhyBc0LPBscDetwnXZosPS2pmtehPSNNsY6Hy1"
 
@@ -11,12 +12,12 @@ def getData(url):
     return response
 
 def getRewards(team):
+    teamString = team
     global year
     data = getData("https://www.thebluealliance.com/api/v3/team/" + team + "/awards/" + year)
-    awards = []
     for award in data:
-        awards.append(award["name"])
-    return awards
+        teamString += "\n\t" + award["name"];
+    teams.append(teamString)
 
 year = input("What year do you want to know the teams orf4450 has competed in: ")
 print("Please wait...")
@@ -24,24 +25,26 @@ print("Please wait...")
 matches = getData("https://www.thebluealliance.com/api/v3/team/frc4450/matches/" + year)
 
 teams = []
-
-print("")
+threads = []
 
 for match in matches:
     redAllianceTeams = match["alliances"]["red"]["team_keys"]
     for team in redAllianceTeams:
         if (not team in teams) and team != "frc4450":
-            teamString = team
-            for award in getRewards(team):
-                teamString += "\n\t" + award
-            teams.append(teamString)
+            thread = threading.Thread(target=getRewards, args=(team,))
+            threads.append(thread)
+            thread.start()
     blueAllianceTeams = match["alliances"]["blue"]["team_keys"]
     for team in blueAllianceTeams:
         if (not team in teams) and team != "frc4450":
-            teamString = team
-            for award in getRewards(team):
-                teamString += "\n\t" + award
-            teams.append(teamString)
+            thread = threading.Thread(target=getRewards, args=(team,))
+            threads.append(thread)
+            thread.start()
+
+for thread in threads:
+    thread.join()
+
+print("")
 
 for team in teams:
     print(team)
