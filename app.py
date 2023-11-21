9a02307ddf4bcdbd846504ff7b80672d2508aad6
Eli Michael <michaelet@students.osd.wednet.edu>
2023-11-20 18:02:22 -0800

    Started the GUI

--- app.py
+++ app.py
@@ -3,6 +3,8 @@
 import requests
 import json
 import threading
+import tkinter
+import tkinter.simpledialog
 
 key = "V86v838SJb4GJhpaNbElRqLSLHFhyBc0LPBscDetwnXZosPS2pmtehPSNNsY6Hy1"
 
@@ -12,42 +14,63 @@ def getData(url):
     return response
 
 def getRewards(team):
-    teamString = team
     global year
+    teamString = team
     data = getData("https://www.thebluealliance.com/api/v3/team/" + team + "/awards/" + year)
     for award in data:
         teamString += "\n\t" + award["name"];
     teams.append(teamString)
 
-year = input("What year do you want to know the teams orf4450 has competed in: ")
-print("Please wait...")
+def getTeamData():
+    global isFinished
+    matches = getData("https://www.thebluealliance.com/api/v3/team/frc4450/matches/" + year)
 
-matches = getData("https://www.thebluealliance.com/api/v3/team/frc4450/matches/" + year)
+    toBeRequestedTeams = []
+    threads = []
 
-teams = []
-toBeRequestedTeams = []
-threads = []
+    for match in matches:
+        redAllianceTeams = match["alliances"]["red"]["team_keys"]
+        for team in redAllianceTeams:
+            if (not team in toBeRequestedTeams) and team != "frc4450":
+                thread = threading.Thread(target=getRewards, args=(team,))
+                threads.append(thread)
+                thread.start()
+                toBeRequestedTeams.append(team)
+        blueAllianceTeams = match["alliances"]["blue"]["team_keys"]
+        for team in blueAllianceTeams:
+            if (not team in toBeRequestedTeams) and team != "frc4450":
+                thread = threading.Thread(target=getRewards, args=(team,))
+                threads.append(thread)
+                thread.start()
+                toBeRequestedTeams.append(team)
 
-for match in matches:
-    redAllianceTeams = match["alliances"]["red"]["team_keys"]
-    for team in redAllianceTeams:
-        if (not team in toBeRequestedTeams) and team != "frc4450":
-            thread = threading.Thread(target=getRewards, args=(team,))
-            threads.append(thread)
-            thread.start()
-            toBeRequestedTeams.append(team)
-    blueAllianceTeams = match["alliances"]["blue"]["team_keys"]
-    for team in blueAllianceTeams:
-        if (not team in toBeRequestedTeams) and team != "frc4450":
-            thread = threading.Thread(target=getRewards, args=(team,))
-            threads.append(thread)
-            thread.start()
-            toBeRequestedTeams.append(team)
+    for thread in threads:
+        thread.join()
 
-for thread in threads:
-    thread.join()
+    isFinished = True
 
-print("")
+def waitForFinish():
+    global isFinished
+    if isFinished:
+        mainWindow.after_cancel(waitForFinishInterval)
+        print("")
+        for team in teams:
+            print(team)
 
-for team in teams:
-    print(team)
+mainWindow = tkinter.Tk()
+pleaseWaitLabel = tkinter.Label(mainWindow, text="Please wait...")
+pleaseWaitLabel.pack()
+
+year = tkinter.simpledialog.askinteger(title="Year", prompt="What year do you want to know the awards of the teams orf4450 has competed with?", minvalue=0)
+
+if (year != None):
+    year = str(year)
+    isFinished = False
+    teams = []
+    thread = threading.Thread(target=getTeamData)
+    thread.start()
+    waitForFinishInterval = mainWindow.after(10, waitForFinish)
+else:
+    mainWindow.destroy()
+
+mainWindow.mainloop()
