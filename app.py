55102f5d6a444d7f0a62e03bac9bd53a7c8fdfc5
Eli Michael <michaelet@students.osd.wednet.edu>
2023-11-20 16:40:07 -0800

    Got the awards each team has gotten

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-# Task: find all the teams 4450 will be upagainst by finding each match frc4450 had competed in and look up those matches to find the teams in those matches
+# Task: find all the teams 4450 will be upagainst by finding each match frc4450 had competed in and look up those matches to find the teams in those matches and the rewards
 
 import requests
 import json
@@ -9,6 +9,14 @@ def getData(url):
     response = requests.get(url, headers={"X-TBA-Auth-Key": key})
     response = json.loads(response.text)
     return response
+
+def getRewards(team):
+    global year
+    data = getData("https://www.thebluealliance.com/api/v3/team/" + team + "/awards/" + year)
+    awards = []
+    for award in data:
+        awards.append(award["name"])
+    return awards
 
 year = input("What year do you want to know the teams orf4450 has competed in: ")
 print("Please wait...")
@@ -23,11 +31,17 @@ for match in matches:
     redAllianceTeams = match["alliances"]["red"]["team_keys"]
     for team in redAllianceTeams:
         if (not team in teams) and team != "frc4450":
-            teams.append(team)
+            teamString = team
+            for award in getRewards(team):
+                teamString += "\n\t" + award
+            teams.append(teamString)
     blueAllianceTeams = match["alliances"]["blue"]["team_keys"]
     for team in blueAllianceTeams:
         if (not team in teams) and team != "frc4450":
-            teams.append(team)
+            teamString = team
+            for award in getRewards(team):
+                teamString += "\n\t" + award
+            teams.append(teamString)
 
 for team in teams:
     print(team)
